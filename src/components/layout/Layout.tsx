b0a5358e884181a42242c611ea0f8f4a4c473487
Evgeniy <nikolaev_zheka-zik@bk.ru>
2023-08-18 10:58:19 +0300

    adds redux-toolkit, react-router

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React, {FC, ReactNode} from 'react'
+import styles from './Layout.module.scss'
+
+const Layout: FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Layout
